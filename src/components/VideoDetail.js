9baa27c8520e152e8035601709d39b23846f74f4
Chris <chewning.cl@gmail.com>
2019-01-01 22:20:40 -0600

    video player connected up but has a few bugs

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,9 +5,19 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>
   }
 
+  const VideoSrc= `https://www.youtube.com/embed/${video.id.videoId}`
 
   return (
-    <div>{video.snippet.title}</div>
+    <div>
+      <div className='ui embed'>
+        <iframe src={VideoSrc} />
+      </div>
+
+      <div className='ui segment'>
+        <h4 className='ui header'>{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
   )
 
 }
